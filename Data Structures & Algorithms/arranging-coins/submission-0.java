b9a8ef19c339412125bd927e6cf950e24594b7af
Abhay Kumar Maurya <abh.mrya@gmail.com>
2026-06-27 18:55:37 +0530

    Add: arranging-coins - submission-0

---
+++ Data Structures & Algorithms/arranging-coins/submission-0.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int arrangeCoins(int n) {
+        int count=0;
+        for(int i =1 ; i<= n ; i++){
+            if(n>=0){
+                n = n-i;
+                count++;
+            }
+            else{
+                break;
+            }
+        }
+        return count;
+    }
+}
